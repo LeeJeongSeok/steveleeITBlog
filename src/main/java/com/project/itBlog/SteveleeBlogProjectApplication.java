46e38780d84355500752665d93311b1a8ffcd8e6
stevelee2020siproject <64336577+stevelee2020siproject@users.noreply.github.com>
2021-04-17 12:56:45 +0900

    project create

---
+++ src/main/java/com/project/itBlog/SteveleeBlogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.itBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteveleeBlogProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteveleeBlogProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/itBlog/SteveleeBlogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.itBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteveleeBlogProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
